67f557474b9664f94cc6adabf4cc18063f4b0dff
kimdust <mji1223@naver.com>
2024-02-08 15:42:04 +0900

    about 수정중

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
 gsap.to("#main3d", {
-  y: 20, // y축 방향으로 움직일 거리
-  duration: 1, // 애니메이션의 지속시간
-  repeat: -1, // 무한 반복
-  yoyo: true, // 애니메이션을 왕복하도록 설정
-  ease: "power1.inOut", // 애니메이션의 속도 조절
+  y: 20,
+  duration: 1,
+  repeat: -1,
+  yoyo: true,
+  ease: "power1.inOut",
 });
