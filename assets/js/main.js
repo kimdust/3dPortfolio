58877a19e62f29e60dc686b9921dd6320fad77dd
kimdust <mji1223@naver.com>
2024-02-08 18:29:53 +0900

    img 세팅

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,3 +5,35 @@ gsap.to("#main3d", {
   yoyo: true,
   ease: "power1.inOut",
 });
+
+gsap.to("#about3d", {
+  y: 20,
+  duration: 1,
+  repeat: -1,
+  yoyo: true,
+  ease: "power1.inOut",
+});
+
+var $mousePointer = $("#mouse_pointer"),
+  $clickElements = $("a, label");
+
+function moverCursor(e) {
+  $mousePointer.css({
+    left: e.pageX,
+    top: e.pageY,
+  });
+}
+$clickElements.mouseenter(function () {
+  $mousePointer.addClass("hover");
+});
+$clickElements.mouseleave(function () {
+  $mousePointer.removeClass("hover");
+});
+
+$(window).on("mousemove", moverCursor);
+
+$(document).ready(function () {
+  $("#fullPage").fullpage({
+    autoScrolling: true,
+  });
+});
