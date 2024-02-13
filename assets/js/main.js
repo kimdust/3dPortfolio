f7bc155ebe986dd426cf47d38f975c5be6d63383
kimdust <mji1223@naver.com>
2024-02-13 17:41:26 +0900

    전체 부분 수정

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,12 +4,26 @@ $(document).ready(function () {
   });
 });
 
-gsap.to("#main3d", {
-  y: 20,
-  duration: 1,
-  repeat: -1,
-  yoyo: true,
-  ease: "power1.inOut",
+document.addEventListener("DOMContentLoaded", function () {
+  gsap.set("#main3d img", { y: "-500%" }); // 초기 위치 설정
+
+  gsap.utils.toArray("#main3d img").forEach((img, index) => {
+    gsap.to(img, {
+      y: 0,
+      delay: index * 0.2, // 이미지마다 0.5초씩 지연
+      duration: 0.8,
+      ease: "power2.out", // 원하는 이징 함수 적용
+      onComplete: function () {
+        gsap.to("#main3d", {
+          y: 20,
+          duration: 1,
+          repeat: -1,
+          yoyo: true,
+          ease: "power1.inOut",
+        });
+      },
+    });
+  });
 });
 
 gsap.to("#about3d, .pp3d", {
